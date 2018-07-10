af973ac3c83106f1859b27ad9b310cfaa84861cc
Fernando Pinto <fernandosilvapinto@outlook.com>
2018-07-09 22:02:28 -0300

    metodos de lista e login

--- index.php
+++ index.php
@@ -7,11 +7,28 @@ require_once("config.php");
 //echo json_encode($usuarios);
 ______________________________________________*/
 
+/*__________________________
+//carrega um usuario;
 $root = new Usuario();
-
 $root->loadById(4);
-
 echo $root;
+_________________________*/
 
+/*_______________________
+//carrega uma lista de usuarios;
+$lista = Usuario::getList();
+echo json_encode($lista);
+__________________*/
 
+/*______________________
+//carrega usuario pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
 ?>
+________________________*/
+
+$usuario = new Usuario();
+
+$usuario->login("jose", "123456");
+
+echo $usuario;
